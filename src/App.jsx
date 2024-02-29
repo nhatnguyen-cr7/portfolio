177f93d8bfd8c55afc7c31e26e9f28e5a165abed
nhatnguyen-cr7 <nhat.nguyen-ojt07@devplus.edu.vn>
2024-02-29 18:20:37 +0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <header>
+      <div className='container m-auto px-4 py-6'>
+        <div className='flex justify-between items-center'>
+          <div>
+            <h1 className='font-bold text-xl'>LOGO</h1>
+          </div>
+          <div>
+            <ul className='flex gap-4'>
+              <li>
+                <a className='text-gray-400 hover:text-white cursor-pointer'>Projects</a>
+              </li>
+              <li>
+                <a className='text-gray-400 hover:text-white cursor-pointer'>Technologies</a>
+              </li>
+              <li>
+                <a className='text-gray-400 hover:text-white cursor-pointer'>About me</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+     </header>
+     <main>
+      <section>
+        <div>
+          
+        </div>
+      </section>
+     </main>
+    </>
+  )
+}
+
+export default App
